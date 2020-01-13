2e6f319ead02ea52ca22d1e3369f08c6d57f281b
Подзоров Евгений Петрович <e.podzorov@RFS.LOCAL>
2020-01-13 17:27:59 +0700

    add template

--- quake/quakemap/views.py
+++ quake/quakemap/views.py
@@ -6,4 +6,10 @@ from django.contrib.sessions.models import Session
 def index(request):
     session_key = request.session.session_key
     #cookie_age = request.session.get_session_cookie_age()
-    return HttpResponse('Session key: ' + session_key)
+    #return HttpResponse('Session key: ' + session_key)
+
+    context = {
+        'session_key':session_key,
+    }
+
+    return render(request,'index.html',context)
