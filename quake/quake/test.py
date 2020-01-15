f1e99489a8db45051b0981ea9a8bb6f3fde9671c
Подзоров Евгений Петрович <e.podzorov@RFS.LOCAL>
2020-01-15 09:20:17 +0700

    commite

---
+++ quake/quake/test.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+
+#local_date_time = datetime.datetime.strptime('1970-01-01' + ' 0:0:0.0',"%Y-%m-%d %H:%M:%S.%f")
+#local_date_time = local_date_time + datetime.timedelta(milliseconds=1578959660779)
+#print(local_date_time)
+
+
+coordinates = [56.14,65.15,5.2]
+
+print(coordinates[1])
--- quake/quakemap/sql_db.py
+++ quake/quakemap/sql_db.py
@@ -1,7 +1,7 @@
 import datetime
 import sqlite3
 from sqlite3 import Error
-from quake.quakemap.get_json import get_json_data
+from quake_code.quakemap import get_json
 
 
 def create_db_connection():
@@ -18,7 +18,7 @@ def create_db_connection():
 def insert_eathquake(conn, sql_object):
     sql_string = ''' INSERT INTO quakemap_eathquake(session_id,src,id_eathquake,version
                         ,eathquake_time,lat,lng,mag,depth,nst,region,data_source,create_date
-                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?) ''')
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?) '''
                     # WHERE id_eathquake <> ?
 
     sql_cursor=conn.cursor()
@@ -33,22 +33,23 @@ def create_row():
     features=get_json_data('2020-01-13', '2020-01-14')
 
     for feature in features:
-        properties=feature["properties"]
-        geometry=properties["geometry"]
+        properties=feature['properties']
+        geometry=properties['geometry']
+        coordinates=geometry['coordinates']
 
-        local_date_time=datetime.datetime.strftime(
-            '1970' + ' ' + '0:0:0.0', "%Y-%m-%d %H:%M:%S.%f")
-        local_date_time = local_date_time + datetime.timedelta(milliseconds=properties['time'])
+        local_date_time=datetime.datetime.strptime(
+            '1970-01-01' + ' 0:0:0.0', "%Y-%m-%d %H:%M:%S.%f")
+        local_date_time=local_date_time + datetime.timedelta(milliseconds = properties['time'])
 
         sql_object=(''
             , properties['sources']
             , feature['id']
             , '1'
             , local_date_time
-            , geometry['coordinates'][0]
-            , geometry['coordinates'][1]
+            , coordinates[0]
+            , coordinates[1]
             , properties['mag']
-            , geometry['coordinates'][2]
+            , coordinates[2]
             , properties['nst']
             , properties['place']
             , 'usgs-gov'
@@ -57,3 +58,6 @@ def create_row():
 
         with conn:
             row_id = insert_eathquake(conn,sql_object)
+
+if __name__ == "__main__":
+    create_row()
